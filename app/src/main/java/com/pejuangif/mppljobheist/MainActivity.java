c67a7758101d00cd6732a022b07870f888c6affa
suyatna <antasuy@gmail.com>
2019-06-20 23:13:04 +0700

    Adding UI Login, and Fragment Login Pencari Kerja

--- app/src/main/java/com/pejuangif/mppljobheist/MainActivity.java
+++ app/src/main/java/com/pejuangif/mppljobheist/MainActivity.java
@@ -1,13 +1,29 @@
 package com.pejuangif.mppljobheist;
 
+import android.content.pm.ActivityInfo;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.pejuangif.mppljobheist.login_pencarikerja.LoginPencariKerjaFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    Fragment fragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        Fragment fragment = new LoginPencariKerjaFragment();
+        replaceFragment(fragment);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
+    }
+
+    void replaceFragment(Fragment fragment) {
+        this.fragment = fragment;
+        getSupportFragmentManager().beginTransaction().replace(R.id.rl_activity_main, fragment).commit();
     }
 }
---
+++ app/src/main/java/com/pejuangif/mppljobheist/base/BaseApplication.java
@@ -0,0 +1,14 @@
+package com.pejuangif.mppljobheist.base;
+
+import android.app.Application;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fresco.initialize(this);
+    }
+}
