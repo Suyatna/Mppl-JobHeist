cb41074602d0bb8b81343d071991e7264f32705e
suyatna <antasuy@gmail.com>
2019-06-21 20:26:38 +0700

    Adding ScrollView, And Daftar section

--- app/src/main/java/com/pejuangif/mppljobheist/MainActivity.java
+++ app/src/main/java/com/pejuangif/mppljobheist/MainActivity.java
@@ -1,29 +1,22 @@
 package com.pejuangif.mppljobheist;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.pejuangif.mppljobheist.login_pencarikerja.LoginPencariKerjaFragment;
+import com.pejuangif.mppljobheist.login.LoginActivity;
+import com.pejuangif.mppljobheist.login.LoginFragment;
 
 public class MainActivity extends AppCompatActivity {
-
-    Fragment fragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().hide();
-        Fragment fragment = new LoginPencariKerjaFragment();
-        replaceFragment(fragment);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
-    }
-
-    void replaceFragment(Fragment fragment) {
-        this.fragment = fragment;
-        getSupportFragmentManager().beginTransaction().replace(R.id.rl_activity_main, fragment).commit();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
