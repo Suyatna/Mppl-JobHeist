02214bf32e24606a1cd95bb5a4c522e31b55c59c
taufikh0112 <taufikh011297@gmail.com>
2019-07-21 13:29:49 +0700

    Update RegisterPemberiKerjaActivity.java

--- app/src/main/java/com/pejuangif/mppljobheist/pemberikerja/RegisterPemberiKerjaActivity.java
+++ app/src/main/java/com/pejuangif/mppljobheist/pemberikerja/RegisterPemberiKerjaActivity.java
@@ -1,10 +1,14 @@
 package com.pejuangif.mppljobheist.pemberikerja;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.pejuangif.mppljobheist.R;
+
+import java.util.Objects;
 
 public class RegisterPemberiKerjaActivity extends AppCompatActivity {
 
@@ -12,5 +16,18 @@ public class RegisterPemberiKerjaActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_pemberi_kerja);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Daftar Pemberi Kerja");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
