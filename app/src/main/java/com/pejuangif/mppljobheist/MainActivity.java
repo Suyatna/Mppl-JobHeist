84d86011c24423c736f906e1d57e205d06afae7d
suyatna <antasuy@gmail.com>
2019-07-31 12:09:56 +0700

    fixing gradle and XLM error

--- app/src/main/java/com/pejuangif/mppljobheist/MainActivity.java
+++ app/src/main/java/com/pejuangif/mppljobheist/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Intent intent = new Intent(MainActivity.this, PekerjaActivity.class);
         startActivity(intent);
         finish();
